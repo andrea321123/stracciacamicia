6470a05d34f4c70194ae007c492da8ce8dd19ca9
andrea321123 <gal.andrea.van@gmail.com>
2021-04-09 16:53:17 +0200

    Remove debug print statements

--- src/game.c
+++ src/game.c
@@ -76,31 +76,20 @@ enum game_result play_game(char *cards) {
     int turn = 0;
     
     while (1) {
-        print_queue(&players[0]);
-        print_queue(&players[1]);
-        printf(" %d  ", turn);
-
-        enum turn_result result = play_turn(turn);
-
-        switch (result) {
+        switch (play_turn(turn);) {
             case TURN_PLAYER_0:
                 transfer_queue(&players[0], &game_queue);
                 turn = 0;
-                printf("L0\n");
                 break;
             case TURN_PLAYER_1:
                 transfer_queue(&players[1], &game_queue);
                 turn = 1;
-                printf("L1\n");
                 break;
             case TURN_INFINITE_GAME:
-            printf("L2\n");
                 return INFINITE_GAME;
             case TURN_WIN_PLAYER_0:
-            printf("L3\n");
                 return WIN_PLAYER_0;
             case TURN_WIN_PLAYER_1:
-            printf("L4\n");
                 return WIN_PLAYER_1;    
             default:
                 break;
